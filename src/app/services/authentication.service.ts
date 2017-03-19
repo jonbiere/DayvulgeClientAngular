16c75645475149df24afc4082655c3049ec3f20e
Jon Biere <jon.biere@gmail.com>
2017-03-19 02:26:09 -0500

    updating authentication service to grab profile pic from login provider

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,142 +1,154 @@
 import { Injectable } from '@angular/core';
-import { AngularFire, AuthProviders, AuthMethods, FirebaseAuthState} from 'angularfire2';
+import { AngularFire, AuthProviders, AuthMethods, FirebaseAuthState } from 'angularfire2';
 import { BehaviorSubject, Observable, Observer } from 'rxjs';
-import { ToasterService} from './toastr.service';
+import { ToasterService } from './toastr.service';
 import { FirebaseRefService } from './firebaseRef.service';
-import {ErrorCodeService, ErrorCodes} from './errorcode.service'
-import {HelperService} from './helper.service';
-import {Profile} from '../viewModels';
-import {AppSettings} from '../appSettings';
+import { ErrorCodeService, ErrorCodes } from './errorcode.service'
+import { HelperService } from './helper.service';
+import { Profile } from '../viewModels';
+import { AppSettings } from '../appSettings';
 import * as firebase from 'firebase';
 
 //declare var jQuery:any;
 @Injectable()
 export class AuthenticationService {
-    currentuser:BehaviorSubject<firebase.User> = new BehaviorSubject<firebase.User>(null);
+    currentuser: BehaviorSubject<firebase.User> = new BehaviorSubject<firebase.User>(null);
 
-    constructor(public af: AngularFire, public toastr:ToasterService, public errorCodeService: ErrorCodeService, public firebaseRefService: FirebaseRefService, public helperService:HelperService) {
+    constructor(public af: AngularFire, public toastr: ToasterService, public errorCodeService: ErrorCodeService, public firebaseRefService: FirebaseRefService, public helperService: HelperService) {
         af.auth.subscribe(authState => {
-            if(!authState){
+            if (!authState) {
                 this.currentuser.next(null);
             }
-            else if(authState.provider === AuthProviders.Password){
-                if(!authState.auth.emailVerified){
+            else if (authState.provider === AuthProviders.Password) {
+                if (!authState.auth.emailVerified) {
                     this.currentuser.next(null);
                     this.toastr.info(`Pleave verify your account to continue. An email has been sent to ${authState.auth.email}`);
                 }
-                else{
+                else {
                     //jQuery('#loginModal').modal('hide');
                     this.currentuser.next(authState.auth);
                 }
-            } 
-            else{
+            }
+            else {
                 //jQuery('#loginModal').modal('hide');
                 this.createUserProfileIfNeeded(authState);
                 this.currentuser.next(authState.auth);
-                
-            }   
-        });
-     }
 
-     login(provider:string, loginModel:any):firebase.Promise<FirebaseAuthState>{
+            }
+        });
+    }
+
+    login(provider: string, loginModel: any): firebase.Promise<FirebaseAuthState> {
         var authProvider: AuthProviders;
         var authMethod: AuthMethods;
-        switch(provider){
+        switch (provider) {
             case 'facebook':
                 authProvider = AuthProviders.Facebook;
                 authMethod = AuthMethods.Redirect;
-            break;
+                break;
             case 'google':
                 authProvider = AuthProviders.Google;
                 authMethod = AuthMethods.Redirect;
-            break;
+                break;
             case 'email':
                 authProvider = AuthProviders.Password;
                 authMethod = AuthMethods.Password;
-            break;
+                break;
             default:
                 return;
         }
-        
-        if(authProvider != AuthProviders.Password){
+
+        if (authProvider != AuthProviders.Password) {
             return this.af.auth.login({
                 provider: authProvider,
                 method: authMethod
             });
         }
-        else{
+        else {
             return this.af.auth.login(loginModel, {
                 provider: authProvider,
                 method: authMethod
             });
         }
 
-        
-     }
 
-     logout(){
-         this.af.auth.logout();
-     }
+    }
 
-     createAccount(registerModel:any):Observable<FirebaseAuthState>{
-        return Observable.fromPromise(<Promise<FirebaseAuthState>> this.af.auth.createUser(registerModel).then(authState=>{
+    logout() {
+        this.af.auth.logout();
+    }
+
+    createAccount(registerModel: any): Observable<FirebaseAuthState> {
+        return Observable.fromPromise(<Promise<FirebaseAuthState>>this.af.auth.createUser(registerModel).then(authState => {
             this.createUserProfileIfNeeded(authState);
         }));
-     }
+    }
 
-     createUserProfileIfNeeded(authState:FirebaseAuthState){
-        let user = authState.auth;   
-        this.firebaseRefService.getCurrentUserProfile(user.uid, false).take(1).subscribe(userProfile => {     
-            if(!userProfile.$exists()){
+    createUserProfileIfNeeded(authState: FirebaseAuthState) {
+        let user = authState.auth;
+        this.firebaseRefService.getCurrentUserProfile(user.uid, false).take(1).subscribe(userProfile => {
+            //set dayvulge user profile
+            if (!userProfile.$exists()) {
                 let newPofile = new Profile(user.email, firebase.database['ServerValue']['TIMESTAMP'], AppSettings.VotesPerDay);
                 this.firebaseRefService.getCurrentUserProfile(user.uid, false).set(newPofile);
             }
-            let displayName = user.email.split('@')[0];
-            let letter = user.email[0].toLowerCase();
-            let photoFile = this.helperService.isLetter(letter) ? letter : 'unknown';
-            user.updateProfile({
-                displayName:displayName,
-                photoURL: `/assets/images/profile/${photoFile}.svg`
-            });
-        });       
+            //set firebase user profile
+            if (authState.provider === AuthProviders.Password) {
+                let displayName = user.email.split('@')[0];
+                let letter = user.email[0].toLowerCase();
+                let photoFile = this.helperService.isLetter(letter) ? letter : 'unknown';
+                user.updateProfile({
+                    displayName: displayName,
+                    photoURL: `/assets/images/profile/${photoFile}.svg`
+                });
+            }
+
+            if (authState.provider === AuthProviders.Facebook) {
+                let fbData = user.providerData[0];
+                user.updateProfile({
+                    displayName: fbData.displayName,
+                    photoURL: fbData.photoURL
+                });
+            }
+        });
     }
 
-     sendPasswordResetEmail(email:string):Observable<boolean>{
-        var result:Observer<boolean>;
+    sendPasswordResetEmail(email: string): Observable<boolean> {
+        var result: Observer<boolean>;
         let auth = firebase.auth();
-        auth.fetchProvidersForEmail(email).then( providers => {       
-            if(providers && providers.length){
-                if(providers[0] === AuthProviders.Password || providers[0] === "password"){
-                    return auth.sendPasswordResetEmail(email).then(sent =>{
+        auth.fetchProvidersForEmail(email).then(providers => {
+            if (providers && providers.length) {
+                if (providers[0] === AuthProviders.Password || providers[0] === "password") {
+                    return auth.sendPasswordResetEmail(email).then(sent => {
                         this.toastr.info(`An email has been sent to ${email}`);
                         result.next(true);
                     }, error => {
                         let e = error as any;
-                        let code:string = e.code || 'auth/internal-error'
+                        let code: string = e.code || 'auth/internal-error'
                         this.toastr.error(this.errorCodeService.getErrorMessage(ErrorCodes[code]));
                         result.next(false);
                     });
                 }
-                else{
+                else {
                     result.next(false);
                     this.toastr.error(this.errorCodeService.getErrorMessage(ErrorCodes['auth/wrong-auth-provider']));
                 }
-            } 
-            else{
+            }
+            else {
                 result.next(false);
                 this.toastr.error(this.errorCodeService.getErrorMessage(ErrorCodes['auth/user-not-found']));
-            }           
+            }
         },
-          error => {  
-            this.toastr.error(error.message);
-            result.next(false);
-          }
+            error => {
+                this.toastr.error(error.message);
+                result.next(false);
+            }
         );
         return Observable.create(observer => result = observer);
-     }
+    }
 
-     getCurrentUser():BehaviorSubject<firebase.User>{
+    getCurrentUser(): BehaviorSubject<firebase.User> {
         return this.currentuser;
-     }
+    }
 
 }
